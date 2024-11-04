41cf6c208dfed43d2eb0071bfda8297ddd3621b0
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-11-04 08:58:16 +0200

    test addride

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api/v1",
-  //baseURL: "https://ballon-booking-backend.vercel.app/api/v1",
+  //baseURL: "http://localhost:5001/api/v1",
+  baseURL: "https://ballon-booking-backend.vercel.app/api/v1",
 });
 
 export default api;
